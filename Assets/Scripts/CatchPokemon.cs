1bca5d3cf8e0099f0a78c4b834c432c17af7f528
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2017-11-02 14:27:45 +0530

    catchPokemon script throw pokeball

---
+++ Assets/Scripts/CatchPokemon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchPokemon : MonoBehaviour {
+
+	public GameObject prefab;
+	void Start () {
+		prefab = Resources.Load ("PokeBall") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject pokeball = Instantiate (prefab) as GameObject;
+			pokeball.transform.position = transform.position + Camera.main.transform.forward * 2;
+			Rigidbody rigidBody = pokeball.GetComponent<Rigidbody> ();
+			rigidBody.velocity = Camera.main.transform.forward * 40;
+		}
+	}
+}
